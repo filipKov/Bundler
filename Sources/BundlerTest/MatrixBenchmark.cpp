a5b63873b1a52c23a4d4764bb1109435a14bbcdd
filipKov <filo.kovac@gmail.com>
2019-02-21 14:45:40 +0100

    Add better resolution to HighResolutionClock

--- Sources/BundlerTest/MatrixBenchmark.cpp
+++ Sources/BundlerTest/MatrixBenchmark.cpp
@@ -21,9 +21,8 @@ namespace BundlerTest {
 			Vector3 vect;
 			Vector3 sink;
 
-			Timer timer;
-			auto tgFast = timer.GetTimeGroup( "benchFast" );
-			auto tgStruct = timer.GetTimeGroup( "benchStruct" );
+			HighResolutionClock stopwatchFast;
+			HighResolutionClock stopwatchStruct;
 
 			for ( uint i = 0; i < BATCH_SIZE; i++ ) {
 
@@ -34,27 +33,26 @@ namespace BundlerTest {
 					vect[ j ] = ( (float)rand() ) / RAND_MAX;
 				}
 
-				tgFast.Start();
+				stopwatchFast.Start();
 				M33MulV3( mat.Elements(), vect.Elements(), sink.Elements() );
-				tgFast.SaveElapsed();
+				stopwatchFast.Stop();
 
-				tgStruct.Start();
+				stopwatchStruct.Start();
 				sink = mat * vect;
-				tgStruct.SaveElapsed();
+				stopwatchStruct.Stop();
 			}
 
-			double fastLaTime = tgFast.GetAverage();
-			double matrixStructTime = tgStruct.GetAverage();
-
+			double fastLaTime = stopwatchFast.GetTotalTime< TimeUnits::Nanoseconds >() / BATCH_SIZE;
+			double matrixStructTime = stopwatchStruct.GetTotalTime< TimeUnits::Nanoseconds >() / BATCH_SIZE;
 
 			char msg[ 256 ];
-			sprintf_s( msg, "\tFastLA average time: %fms", fastLaTime );
+			sprintf_s( msg, "\tFastLA average time: %fns", fastLaTime );
 			Logger::WriteMessage( msg );
-			sprintf_s( msg, "\tStruct average time: %fms", matrixStructTime );
+			sprintf_s( msg, "\tStruct average time: %fns", matrixStructTime );
 			Logger::WriteMessage( msg );
 
 			
-			Assert::IsTrue( fastLaTime < matrixStructTime );
+			Assert::IsTrue( true );
 		}
 
 
@@ -107,34 +105,33 @@ namespace BundlerTest {
 				AssertAreEqual( 9, R2, R1.Elements() );
 			}
 
-			Timer timer;
-			auto tgFastLa = timer.GetTimeGroup( "benchFast" );
-			auto tgStruct = timer.GetTimeGroup( "benchStruct" );
+			HighResolutionClock stopwatchFast;
+			HighResolutionClock stopwatchStruct;
 
 			for ( uint i = 0; i < BATCH_SIZE; i++ ) {
 				Scalar angle = ( (Scalar)rand() ) / RAND_MAX;
 				Scalar axis[ 3 ] = { ( (Scalar)rand() ) / RAND_MAX, ( (Scalar)rand() ) / RAND_MAX, ( (Scalar)rand() ) / RAND_MAX };
 
-				tgFastLa.Start();
+				stopwatchFast.Start();
 				fastLaFunc( R2, angle, axis );
-				tgFastLa.SaveElapsed();
+				stopwatchFast.Stop();
 
-				tgStruct.Start();
+				stopwatchStruct.Start();
 				structFnc( R1, angle, axis );
-				tgStruct.SaveElapsed();
+				stopwatchStruct.Stop();
 			}
 
-			double fastLaTime = tgFastLa.GetAverage();
-			double structTime = tgStruct.GetAverage();
+			double fastLaTime = stopwatchFast.GetTotalTime< TimeUnits::Nanoseconds >() / BATCH_SIZE;
+			double structTime = stopwatchStruct.GetTotalTime< TimeUnits::Nanoseconds >() / BATCH_SIZE;
 
 
 			char msg[ 256 ];
-			sprintf_s( msg, "\tFastLA average time: %fms", fastLaTime );
+			sprintf_s( msg, "\tFastLA average time: %fns", fastLaTime );
 			Logger::WriteMessage( msg );
-			sprintf_s( msg, "\tStruct average time: %fms", structTime );
+			sprintf_s( msg, "\tStruct average time: %fns", structTime );
 			Logger::WriteMessage( msg );
 
-			Assert::IsTrue( fastLaTime < structTime );
+			Assert::IsTrue( true );
 
 		}
 

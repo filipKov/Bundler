179608423136b7bb182122250e8a5a522174ad21
filipKov <filo.kovac@gmail.com>
2019-04-04 18:33:25 +0200

    Add ListAccelerators function

--- Sources/Bundler/AMPUtils.h
+++ Sources/Bundler/AMPUtils.h
@@ -40,4 +40,27 @@ namespace Bundler { namespace Async {
 		}
 	}
 
+	template < typename Predicate >
+	void ListAccelerators( __in Predicate pred )
+	{
+		std::vector < accelerator > accelerators = accelerator::get_all( );
+
+		for ( auto it = accelerators.begin( ); it != accelerators.end( ); it++ )
+		{
+			if ( pred( *it ) )
+			{
+				wprintf_s( L"Accelerator: %s\n", it->get_description( ).c_str( ) );
+				wprintf_s( L"\tDevice path: %s\n", it->get_device_path( ).c_str( ) );
+				wprintf_s( L"\tDedicated memory: %zuKB\n", it->get_dedicated_memory( ) );
+				wprintf_s( L"\tHas attached display: %s\n", it->get_has_display( ) ? L"True" : L"False" );
+				wprintf_s( L"\tIs emulated: %s\n", it->get_is_emulated( ) ? L"True" : L"False" );
+				wprintf_s( L"\tSupports shared memory: %s\n", it->get_supports_cpu_shared_memory( ) ? L"True" : L"False" );
+				wprintf_s( L"\tSupports double precision: %s\n", it->get_supports_double_precision( ) ? L"True" : L"False" );
+				wprintf_s( L"\tSupports limited double precision: %s\n", it->get_supports_limited_double_precision( ) ? L"True" : L"False" );
+				wprintf_s( L"\tVersion: %u\n", it->get_version( ) );
+				wprintf_s( L"\n" );
+			}
+		}
+	}
+
 } }

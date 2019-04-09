a41ebd6267eab995cb9c78c239fa01991fc5e904
filipKov <filo.kovac@gmail.com>
2019-04-09 23:53:27 +0200

    Add AMP versions of static Matrix/Vector methods and functions

--- Sources/LinearAlgebra/StaticVector.h
+++ Sources/LinearAlgebra/StaticVector.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include "stdafx.h"
-#include "StaticMatrix.h"
-
 namespace LinearAlgebra {
 
 	template< typename Scalar, int dimension = MatrixBase::Dynamic >
@@ -13,9 +10,12 @@ namespace LinearAlgebra {
 		// -------------------------------------------------------------------------------------------------------
 		//    Constructors/Initializers/Destructors
 		// -------------------------------------------------------------------------------------------------------
-		Vector() : Matrix<Scalar, dimension, 1>() {};
+		Vector() 
+			: Matrix<Scalar, dimension, 1>() 
+		{};
 
-		Vector( __in const std::initializer_list<Scalar> elements ) : Matrix<Scalar, dimension, 1>( ) {
+		Vector( __in const std::initializer_list<Scalar> elements ) : Matrix<Scalar, dimension, 1>( ) 
+		{
 			if ( elements.size() != dimension ) {
 				throw InvalidArgumentException( GET_VARIABLE_NAME( elements ), ExceptionReasons::DIMENSIONS_MISMATCH );
 			}
@@ -23,35 +23,35 @@ namespace LinearAlgebra {
 			ShallowCopy<Scalar>( elements.begin(), static_cast<uint>( elements.size() ), m_pData );
 		};
 
-		Vector( __in const Scalar (&values)[ dimension ] ) {
+		Vector( __in const Scalar (&values)[ dimension ] ) 
+		{
 			ShallowCopy< Scalar >( values, dimension, m_pData );
 		}
 
-		Vector( __in const Matrix< Scalar, dimension, 1 >& src ) : Matrix< Scalar, dimension, 1 >( src ) {};
-
-		~Vector() {};
+		Vector( __in const Matrix< Scalar, dimension, 1 >& src ) 
+			: Matrix< Scalar, dimension, 1 >( src ) 
+		{};
 
 		// -------------------------------------------------------------------------------------------------------
 		//    Accessors overrides
 		// -------------------------------------------------------------------------------------------------------
-		inline const Scalar& operator[] ( __in const uint i ) const {
-			if ( i >= static_cast<uint>( dimension ) ) {
-				throw IndexOutOfRangeException( i, static_cast<uint>( dimension - 1 ) );
-			}
+		inline const Scalar& operator[] ( __in const uint i ) const 
+		{
+			_ASSERT_EXPR( i < ( uint )dimension, "Index out of range" );
 
 			return *( m_pData + i );
 		}
 
-		inline Scalar& operator[] ( __in const uint i ) {
-			if ( i >= static_cast<uint>( dimension ) ) {
-				throw IndexOutOfRangeException( i, static_cast<uint>( dimension - 1 ) );
-			}
+		inline Scalar& operator[] ( __in const uint i ) 
+		{
+			_ASSERT_EXPR( i < ( uint )dimension, "Index out of range" );
 
 			return *( m_pData + i );
 		}
 
 
-		inline Vector<Scalar, dimension>& operator= ( __in const Matrix<Scalar, dimension, 1>& src ) {
+		inline Vector<Scalar, dimension>& operator= ( __in const Matrix<Scalar, dimension, 1>& src ) 
+		{
 			ShallowCopy<Scalar>( src.Elements(), dimension, m_pData );
 			return *this;
 		};
@@ -59,15 +59,14 @@ namespace LinearAlgebra {
 		// -------------------------------------------------------------------------------------------------------
 		//    Other
 		// -------------------------------------------------------------------------------------------------------
-		inline void Normalize() {
-			Scalar norm = ( *this ) * ( *this );
-			norm = 1 / sqrt( norm );
-			for ( uint i = 0; i < dimension; i++ ) {
-				m_pData[i] *= norm;
-			}
+		inline void Normalize() 
+		{
+			Scalar norm = Scalar( 1 ) / sqrt( VectorDot< Scalar, dimension >( m_pData, m_pData ) );
+			MatrixMultiplyC< Scalar, dimension, 1 >( m_pData, norm, m_pData );
 		}
 
-		inline Scalar DistanceSquared( __in const Vector< Scalar, dimension >& B ) const {
+		inline Scalar DistanceSquared( __in const Vector< Scalar, dimension >& B ) const 
+		{
 			Scalar dist = Scalar( 0 );
 			for ( uint i = 0; i < dimension; i++ ) {
 				dist += ( m_pData[i] - B.m_pData[i] ) * ( m_pData[i] - B.m_pData[i] );
@@ -76,11 +75,76 @@ namespace LinearAlgebra {
 			return dist;
 		}
 
-		inline Scalar Distance( __in const Vector< Scalar, dimension >& B ) const {
+		inline Scalar Distance( __in const Vector< Scalar, dimension >& B ) const 
+		{
 			return sqrt( DistanceSquared( B ) );
 		}
 
-	};
+	#ifdef IMPORT_AMP_LINEAR_ALGEBRA
 
+		// -------------------------------------------------------------------------------------------------------
+		//    Constructors/Initializers/Destructors
+		// -------------------------------------------------------------------------------------------------------
+		Vector( ) restrict( amp )
+			: Matrix<Scalar, dimension, 1>( )
+		{};
+
+		Vector( __in const Scalar ( &values )[dimension] ) restrict( amp )
+		{
+			Containers::ArrayUtils< Scalar >::Copy< dimension >( values, m_pData );
+		}
+
+		Vector( __in const Matrix< Scalar, dimension, 1 >& src ) restrict( amp )
+			: Matrix< Scalar, dimension, 1 >( src )
+		{
+		};
+
+		// -------------------------------------------------------------------------------------------------------
+		//    Accessors overrides
+		// -------------------------------------------------------------------------------------------------------
+		inline const Scalar& operator[] ( __in const uint i ) const restrict( amp )
+		{
+			return *( m_pData + i );
+		}
+
+		inline Scalar& operator[] ( __in const uint i ) restrict( amp )
+		{
+			return *( m_pData + i );
+		}
+
+		inline Vector<Scalar, dimension>& operator= ( __in const Matrix<Scalar, dimension, 1>& src ) restrict( amp )
+		{
+			Containers::ArrayUtils< Scalar >::Copy< dimension >( src.Elements(), m_pData );
+			return *this;
+		};
+
+		// -------------------------------------------------------------------------------------------------------
+		//    Other
+		// -------------------------------------------------------------------------------------------------------
+		inline void Normalize( ) restrict( amp )
+		{
+			Scalar norm = Scalar( 1 ) / concurrency::precise_math::sqrt( VectorDot< Scalar, dimension >( m_pData, m_pData ) );
+			MatrixMultiplyC< Scalar, dimension, 1 >( m_pData, norm, m_pData );
+		}
+
+		inline Scalar DistanceSquared( __in const Vector< Scalar, dimension >& B ) const restrict( amp )
+		{
+			Scalar dist = Scalar( 0 );
+			for ( uint i = 0; i < dimension; i++ )
+			{
+				dist += ( m_pData[i] - B.m_pData[i] ) * ( m_pData[i] - B.m_pData[i] );
+			}
+
+			return dist;
+		}
+
+		inline Scalar Distance( __in const Vector< Scalar, dimension >& B ) const restrict( amp )
+		{
+			return concurrency::precise_math::sqrt( DistanceSquared( B ) );
+		}
+
+	#endif
+
+	};
 
 }
--- Sources/LinearAlgebra/StaticVectorOperators.h
+++ Sources/LinearAlgebra/StaticVectorOperators.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include "stdafx.h"
-#include "StaticVector.h"
-
 namespace LinearAlgebra {
 
 	template < typename Scalar, uint rows, uint cols >
@@ -11,15 +8,7 @@ namespace LinearAlgebra {
 		__in const Vector<Scalar, cols >& vector )
 	{
 		Vector< Scalar, rows > res;
-
-		for ( uint r = 0; r < rows; r++ ) {
-			Scalar tmp = Scalar( 0 );
-			for ( uint c = 0; c < cols; c++ ) {
-				tmp += matrix[r][c] * vector[c];
-			}
-			res[r] = tmp;
-		}
-
+		MatrixMultiply< Scalar, rows, cols, 1 >( matrix.Elements( ), vector.Elements( ), res.Elements( ) );
 		return res;
 	};
 
@@ -38,22 +27,54 @@ namespace LinearAlgebra {
 		__in const Vector<Scalar, dimension>& vLeft,
 		__in const Vector<Scalar, dimension>& vRight )
 	{
-		Scalar dot = Scalar( 0 );
-		for ( uint i = 0; i < dimension; i++ )
-		{
-			dot += vLeft[i] * vRight[i];
-		}
-
-		return dot;
+		return VectorDot< Scalar, dimension >( vLeft.Elements( ), vRight.Elements( ) );
 	};
 
 	template < typename Scalar >
 	inline Vector<Scalar, 3> CrossProduct( __in const Vector<Scalar, 3>& vectL, __in const Vector<Scalar, 3>& vectR ) {
-		return Vector<Scalar, 3>( {
-			( vectL[1] * vectR[2] ) - ( vectL[2] * vectR[1] ),
-			( vectL[2] * vectR[0] ) - ( vectL[0] * vectR[2] ),
-			( vectL[0] * vectR[1] ) - ( vectL[1] * vectR[0] )
-		} );
+		Vector< Scalar, 3 > cp;
+		V3Cross( vectL.Elements( ), vectR.Elements( ), cp.Elements( ) );
+		return cp;
 	};
+
+#ifdef IMPORT_AMP_LINEAR_ALGEBRA
+
+	template < typename Scalar, uint rows, uint cols >
+	inline Vector< Scalar, rows > operator* (
+		__in const Matrix<Scalar, rows, cols >& matrix,
+		__in const Vector<Scalar, cols >& vector ) restrict( amp )
+	{
+		Vector< Scalar, rows > res;
+		MatrixMultiply< Scalar, rows, cols, 1 >( matrix.Elements( ), vector.Elements( ), res.Elements( ) );
+		return res;
+	};
+
+	template < typename Scalar, uint dimension >
+	inline Vector< Scalar, dimension > operator+ (
+		__in const Vector<Scalar, dimension>& vLeft,
+		__in const Vector<Scalar, dimension>& vRight ) restrict( amp )
+	{
+		Vector<Scalar, dimension> res( vLeft );
+		res += vRight;
+		return res;
+	};
+
+	template < typename Scalar, uint dimension >
+	inline Scalar operator* (
+		__in const Vector<Scalar, dimension>& vLeft,
+		__in const Vector<Scalar, dimension>& vRight ) restrict( amp )
+	{
+		return VectorDot< Scalar, dimension >( vLeft.Elements( ), vRight.Elements( ) );
+	};
+
+	template < typename Scalar >
+	inline Vector<Scalar, 3> CrossProduct( __in const Vector<Scalar, 3>& vectL, __in const Vector<Scalar, 3>& vectR ) restrict( amp )
+	{
+		Vector< Scalar, 3 > cp;
+		V3Cross( vectL.Elements( ), vectR.Elements( ), cp.Elements( ) );
+		return cp;
+	};
+
+#endif
 
 }

cd7476394a8d214e858c2c0beeee09d234551197
filipKov <filo.kovac@gmail.com>
2019-04-11 17:28:58 +0200

    Add AMP versions of Bundler Utils

--- Sources/Bundler/BundlerUtils.h
+++ Sources/Bundler/BundlerUtils.h
@@ -9,7 +9,7 @@ namespace Bundler { namespace Utils {
 	}
 
 	template < class CameraModel >
-	inline Scalar* GetCameraParamPtr( __in const size_t cameraIndex, __in Scalar* pParameterVector )
+	inline Scalar* GetCameraParamPtr( __in const size_t cameraIndex, __in Scalar* pParameterVector ) 
 	{
 		return const_cast<Scalar*>( GetCameraParamPtr< CameraModel >( cameraIndex, (const Scalar*)pParameterVector ) );
 	}
@@ -26,6 +26,33 @@ namespace Bundler { namespace Utils {
 	inline Scalar* GetPointParamPtr( __in const size_t pointIndex, __in const size_t cameraCount, __in Scalar* pParameterVector )
 	{
 		return const_cast<Scalar*>( GetPointParamPtr< CameraModel >( pointIndex, cameraCount, (const Scalar*)pParameterVector ) );
+	}
+
+
+	template < class CameraModel >
+	inline const Scalar* GetCameraParamPtr( __in const uint cameraIndex, __in const Scalar* pParameterVector ) __GPU_ONLY
+	{
+		return pParameterVector + cameraIndex * CameraModel::cameraParameterCount;
+	}
+
+	template < class CameraModel >
+	inline Scalar* GetCameraParamPtr( __in const uint cameraIndex, __in Scalar* pParameterVector ) __GPU_ONLY
+	{
+		return const_cast<Scalar*>( GetCameraParamPtr< CameraModel >( cameraIndex, ( const Scalar* )pParameterVector ) );
+	}
+
+
+	template < class CameraModel >
+	inline const Scalar* GetPointParamPtr( __in const uint pointIndex, __in const uint cameraCount, __in const Scalar* pParameterVector ) __GPU_ONLY
+	{
+		constexpr const uint pointParamCount = CameraModel::totalParamCount - CameraModel::cameraParameterCount;
+		return pParameterVector + cameraCount * CameraModel::cameraParameterCount + pointIndex * pointParamCount;
+	}
+
+	template < class CameraModel >
+	inline Scalar* GetPointParamPtr( __in const uint pointIndex, __in const uint cameraCount, __in Scalar* pParameterVector ) __GPU_ONLY
+	{
+		return const_cast<Scalar*>( GetPointParamPtr< CameraModel >( pointIndex, cameraCount, ( const Scalar* )pParameterVector ) );
 	}
 
 } }

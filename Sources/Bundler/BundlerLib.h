abe8fc5b2dbae3a36cd24d94af62619f2146dfaa
filipKov <filo.kovac@gmail.com>
2019-04-16 13:11:30 +0200

    Remove old tasks for hessian multiplication

--- Sources/Bundler/BundlerLib.h
+++ Sources/Bundler/BundlerLib.h
@@ -52,8 +52,6 @@
 #include "InterlockedVariable.h"
 
 #include "ITask.h"
-#include "HessianCameraRowMultiplicationTask.h"
-#include "HessianPointRowMultiplicationTask.h"
 
 #include "IWorkerThreadCallback.h"
 #include "WorkerThreadDefs.h"
@@ -81,7 +79,7 @@
 
 #include "ParallelPCGInitTasksCPU.h"
 #include "ParallelPCGLoopTasksCPU.h"
-#include "ParallelPCGInitTasksAMP.h"
+// #include "ParallelPCGInitTasksAMP.h" // cannot get it to compile...
 #include "ParallelPCGLoopTasksAMP.h"
 
 #include "ParallelPcgTaskFactory.h"
